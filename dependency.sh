a0db6eee12cdaf557175db331f3a08638b0ad5a4
Joaquin Osvaldo Rodriguez <admin@meritoki.com>
2019-11-16 14:44:20 -0300

    Adding pip install

--- dependency.sh
+++ dependency.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get -y install python
+sudo apt install -y python-pip
 pip install --upgrade cryptography --user
 pip install -U panoptescli --user
 pip install -U pyopenssl --user
