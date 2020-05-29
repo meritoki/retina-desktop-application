f098405cda246aca00d2ce785053ba716a728b33
meritkoi <meritoki@yahoo.com>
2020-05-29 18:07:11 -0300

    All current tests pass

--- src/main/java/com/meritoki/app/desktop/retina/model/document/Page.java
+++ src/main/java/com/meritoki/app/desktop/retina/model/document/Page.java
@@ -200,7 +200,11 @@ public class Page {
 	public BufferedImage getShapeBufferedImage(Shape shape) {
 		BufferedImage bufferedImage = null;
 		if (this.getBufferedImage() != null) {
-//			bufferedImage = this.getBufferedImage().getSubimage((int)shape.position.point.x, (int)shape.position.point.y, (int)shape.position.dimension.width, (int)shape.position.dimension.height);
+			if(shape.position.point.x > 0 && shape.position.point.y > 0 && shape.position.dimension.height > 0 && shape.position.dimension.width > 0) {
+				bufferedImage = this.getBufferedImage().getSubimage((int)shape.position.point.x, (int)shape.position.point.y, (int)shape.position.dimension.width, (int)shape.position.dimension.height);
+			} else {
+				logger.error("MAJOR ERROR THAT NEEDS TO BE FIXED shape.position="+shape.position);
+			}
 		}
 		return bufferedImage;
 	}
