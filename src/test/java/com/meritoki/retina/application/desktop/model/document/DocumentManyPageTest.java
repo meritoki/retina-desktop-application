62d96300e9ff7f2483502f8a28979d5061f79e9a
meritkoi <meritoki@yahoo.com>
2020-05-30 16:10:49 -0300

    Added New Tests Many Page, Image, and Shape Tests

---
+++ src/test/java/com/meritoki/retina/application/desktop/model/document/DocumentManyPageTest.java
@@ -0,0 +1,46 @@
+package com.meritoki.retina.application.desktop.model.document;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import com.meritoki.app.desktop.retina.controller.document.DocumentController;
+import com.meritoki.app.desktop.retina.model.document.Document;
+import com.meritoki.app.desktop.retina.model.document.Image;
+import com.meritoki.app.desktop.retina.model.document.Page;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DocumentManyPageTest {
+
+	static Logger logger = LogManager.getLogger(DocumentResizeShapeTest.class.getName());
+	static Document document = null;
+	int limit = 16;
+	
+	@BeforeAll
+	public static void initialize() {
+		document = new Document();
+	}
+	
+	@Test
+	@Order(1)
+	public void addPages() {
+		for(int i=0;i<limit;i++) {
+			Page page = new Page(new Image(new File("./data/image/01.jpg")));
+			document.addPage(page);
+		}
+	}
+	
+	@Test
+	@Order(2)
+	public void savePages() {
+		DocumentController.save(new java.io.File("./test/document-many-page-test.json"), document);
+	}
+}
