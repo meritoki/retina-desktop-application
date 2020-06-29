b1cf83d0b40019843a0f177c4f9ddd36b9759ffd
meritkoi <meritoki@yahoo.com>
2020-06-29 18:30:48 -0300

    Deleted unnecessary files and folders

--- archive/shell/zooniverse.sh
+++
@@ -1,6 +0,0 @@
-  # echo -e "TEST\dedalo2018\https://www.zooniverse.org" | panoptes configure
-# printf "%s\n" TEST dedalo2018 https://www.zooniverse.org | panoptes configure
-cp config.yml ~/.panoptes/
-
-# panoptes project create "Test" "Testing CLI Interface"
-panoptes project modify --help
