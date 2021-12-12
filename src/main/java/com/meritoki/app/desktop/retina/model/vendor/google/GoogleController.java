222f1b42bed4641918740ad35d6f885d0d3e5acb
Joaquin Osvaldo Rodriguez <admin@meritoki.com>
2021-12-12 17:55:14 -0300

    Set Unit Type & Value Once For All Shapes in Grid

--- src/main/java/com/meritoki/app/desktop/retina/model/vendor/google/GoogleController.java
+++ src/main/java/com/meritoki/app/desktop/retina/model/vendor/google/GoogleController.java
@@ -3,7 +3,6 @@ package com.meritoki.app.desktop.retina.model.vendor.google;
 import com.meritoki.app.desktop.retina.controller.node.NodeController;
 
 public class GoogleController {
-
 	
 	public static String getGoogleHome() {
 		return NodeController.getSystemHome() + NodeController.getSeperator() + "vendor"
