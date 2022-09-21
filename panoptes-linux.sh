a462e72f7c0d6266cb3e4795f994cf523c2f3a56
Joaquin Osvaldo Rodriguez <jorodriguez1988@gmail.com>
2022-09-21 11:44:53 -0300

    Fixing Panoptes Install Scripts for Linux and Windows

--- panoptes-linux.sh
+++ panoptes-linux.sh
@@ -1,12 +1,11 @@
 sudo apt-get update
-sudo apt-get -y install python
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
-sudo python get-pip.py
-sudo apt-get install libpython-dev
-pip install awscli
-pip install -U panoptescli
-sudo pip install --upgrade cryptography
-sudo python -m easy_install --upgrade pyOpenSSL
+sudo apt-get -y install python3
+sudo apt-get install python3-pip -y
+sudo apt-get install libpython3-dev -y
+pip3 install awscli
+pip3 install -U panoptescli
+sudo pip3 install --upgrade cryptography
+sudo python3 -m easy_install --upgrade pyOpenSSL
 BASHRC=~/.bashrc
-grep -qxF "export PY_USER_BIN=\$(python -c 'import site; print(site.USER_BASE + \"/bin\")')" $BASHRC || echo "export PY_USER_BIN=\$(python -c 'import site; print(site.USER_BASE + \"/bin\")')" >> $BASHRC
+grep -qxF "export PY_USER_BIN=\$(python3 -c 'import site; print(site.USER_BASE + \"/bin\")')" $BASHRC || echo "export PY_USER_BIN=\$(python3 -c 'import site; print(site.USER_BASE + \"/bin\")')" >> $BASHRC
 grep -qxF 'export PATH=$PY_USER_BIN:$PATH' $BASHRC || echo 'export PATH=$PY_USER_BIN:$PATH' >> $BASHRC
--- panoptes-windows.ps1
+++ panoptes-windows.ps1
@@ -1,6 +1,6 @@
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
-python get-pip.py
-pip install awscli
+
+pip install -U awscli
 pip install -U panoptescli
-pip install --upgrade cryptography
+pip install python-magic-bin
+pip install -U cryptography==35.0
 python -m easy_install --upgrade pyOpenSSL
